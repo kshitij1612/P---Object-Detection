2b81053e47e3699b5cae4471f843028bd7bca83b
kshitij1612 <80765938+kshitij1612@users.noreply.github.com>
2021-09-17 15:20:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function bedroom() {
+    window.location = "bedroom.html";
+}
+
+function living() {
+    window.location = "living.html";
+}
+
+function balcony() {
+    window.location = "balcony.html";
+}
